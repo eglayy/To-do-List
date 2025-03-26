8af722f09eb8009d47cfebffcb0685dd73aeae31
Egor <egorgood767@gmail.com>
2025-03-26 20:20:02 +0300

    + debug.py for sql queries visualization, add generate_data command for generated to-do lists

--- todolist/settings.py
+++ todolist/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'todolist.debug.SqlPrintingMiddleware'
 ]
 
 ROOT_URLCONF = 'todolist.urls'
